79f4a91ba03df0060892acd6ba73dc0da1f04d20
Kevin Carreño <kevincarrenot@gmail.com>
2020-10-14 19:01:42 -0500

    EnemigosNavMesh

---
+++ Viking Survival Game/Assets/Scripts/pullenemigos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class pullenemigos : MonoBehaviour
+{
+    public GameObject enemigo;
+    public int xPos;
+    public int zPos;
+    public int numEnemigos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemigoDrop());
+    }
+    IEnumerator EnemigoDrop()
+    {
+        while (1 < numEnemigos)
+        {
+            xPos = Random.Range(240, 260);
+            zPos = Random.Range(175, 185);
+            Instantiate(enemigo, new Vector3(xPos, 120, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+            numEnemigos -= 1;
+        }
+    }
+
+
+}
